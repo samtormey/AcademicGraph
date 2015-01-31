d861ec66e4f8b3093a0c91c9df97b04dabb5fac4
samtormey <samueltormey2@gmail.com>
2015-01-31 15:47:58 -0600

    added scrapy project

---
+++ References_Scrap/References_Scrap/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for References_Scrap project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'References_Scrap'
+
+SPIDER_MODULES = ['References_Scrap.spiders']
+NEWSPIDER_MODULE = 'References_Scrap.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'References_Scrap (+http://www.yourdomain.com)'
